66e79626d370e219d41d5352c508801a04ba3e26
Duda <mariaeduarda@gmail.com>
2022-05-12 22:00:46 -0300

    cria o componente botão

--- src/pages/Itens.tsx
+++ src/pages/Itens.tsx
@@ -1,21 +1,25 @@
 import Cabecalho from "./components/cabecalho";
 import Rodape from "./components/rodape"; 
 import './Itens.css';
+import Botao from "./components/botao";
+
 const ItensPage = function() { 
     return (
-      <> 
-        <Cabecalho />
-         <main>        
-          <h1>Itens</h1>
-          <ul>
-           <li>Item 1</li>
-           <li>Item2</li>
-           <li>Item3</li>
-        </ul>
-    </main>
-
-       <Rodape />
-    </>
+    <div>
+            <Cabecalho />
+            <main>        
+                <h1>Itens</h1>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item2</li>
+                    <li>Item3</li>
+                </ul>
+                <Botao texto = "enviar"/><Botao texto= "enviar" />
+               
+          </main>
+          <Rodape />
+          </div>
+       
    );
 };
 
---
+++ src/pages/components/botao.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from 'react';
+import './botao.css';
+type Props = { 
+   texto:string 
+}
+const Botao: FunctionComponent<Props> = function (props){
+   return(
+        <>
+        <button>enviar</button> 
+        </>
+
+
+
+   );
+
+
+};
+
+export default Botao; 
